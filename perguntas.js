87a0268eb665dc9b25cbd7eae6788542d9560013
KDCO1173 <00001117355986sp@al.educacao.sp.gov.br>
2024-11-18 14:43:01 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    'Qual menor país do mundo em área? ',
+    'Vaticano'
+)
+
+criaCartao(
+    'Geral',
+    'Quem pintou a obra "A Última Ceia?"',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'Geral',
+    'Qual o maior orgão do corpo humano?',
+    'Pele'
+)
+
+criaCartao(
+    'Geral',
+    'Qual planeta do Sistema Solar é conhecido como o "planeta vermelho"?',
+    'Marte'
+)
